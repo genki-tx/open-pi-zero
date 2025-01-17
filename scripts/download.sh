035b1a15288b7ea7d55eaa479785b8129f443334
genki-tx <genki.sano@tx-inc.com>
2025-01-17 12:22:28 +0900

    updated download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -11,22 +11,26 @@ mkdir -p ${HF_DATA_DIR}
 mkdir -p ${VLA_DATA_DIR}
 mkdir -p ${VLA_LOG_DIR}
 
+# set download_datasets=true if you want to download, set false if you skip it
 download_datasets=true
 if [ "$download_datasets" = true ]; then
-# download paligemma from hugging face, setup your account with huggingface-cli first, 	https://huggingface.co/docs/huggingface_hub/en/guides/cli
-huggingface-cli download google/paligemma-3b-pt-224 --local-dir ${HF_DATA_DIR}/paligemma-3b-pt-224
+    # download paligemma from hugging face, setup your account with huggingface-cli first, https://huggingface.co/docs/huggingface_hub/en/guides/cli
+    huggingface-cli download google/paligemma-3b-pt-224 --local-dir ${HF_DATA_DIR}/paligemma-3b-pt-224
 
-# download fractal, setup gsutil command, https://cloud.google.com/storage/docs/gsutil_install
-gsutil -m cp -r gs://gresearch/robotics/fractal20220817_data/0.1.0 ${VLA_DATA_DIR}/
+    # download fractal, setup gsutil command, https://cloud.google.com/storage/docs/gsutil_install
+    gsutil -m cp -n -r gs://gresearch/robotics/fractal20220817_data/0.1.0 ${VLA_DATA_DIR}/
 
-# download bridge dataset
-wget -r -np -nH --cut-dirs=3 -R "index.html*" -P ${VLA_DATA_DIR}/bridge_dataset https://rail.eecs.berkeley.edu/datasets/bridge_release/data/tfds/
+    # download bridge dataset
+    wget -r -np -nH --cut-dirs=3 -R "index.html*" -nc -P ${VLA_DATA_DIR}/bridge_dataset https://rail.eecs.berkeley.edu/datasets/bridge_release/data/tfds/
 
 fi # download_datasets
 
 # if you prefer to download pretrain mode, set download_pretrain_model=true 
-download_pretrain_model=false
+download_pretrain_model=true
 if [ "$download_pretrain_model" = true ]; then
-    wget -P ${VLA_LOG_DIR} https://huggingface.co/allenzren/open-pi-zero/resolve/main/bridge_uniform_step19296_2024-12-26_22-31_42.pt
+    wget -nc -P ${VLA_LOG_DIR} https://huggingface.co/allenzren/open-pi-zero/resolve/main/bridge_uniform_step19296_2024-12-26_22-31_42.pt
+    wget -nc -P ${VLA_LOG_DIR} https://huggingface.co/allenzren/open-pi-zero/resolve/main/bridge_beta_step19296_2024-12-26_22-30_42.pt
+    wget -nc -P ${VLA_LOG_DIR} https://huggingface.co/allenzren/open-pi-zero/resolve/main/fractal_uniform_step29576_2024-12-31_22-26_42.pt
+    wget -nc -P ${VLA_LOG_DIR} https://huggingface.co/allenzren/open-pi-zero/resolve/main/fractal_beta_step29576_2024-12-29_13-10_42.pt
 fi
 
