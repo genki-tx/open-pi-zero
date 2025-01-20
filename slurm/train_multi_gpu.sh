ce9cfdeeb7708d6e5269a834fc9b6b38cc1d1d23
genki-tx <genki.sano@tx-inc.com>
2025-01-20 01:05:13 +0000

    training with multiple GPU working

--- slurm/train_multi_gpu.sh
+++ slurm/train_multi_gpu.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 
-#SBATCH --job-name=pg-vla
-#SBATCH --output=logs/%A.out
-#SBATCH --error=logs/%A.err
-#SBATCH --time=71:59:59
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:8
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=104
-#SBATCH --mem=500G
-
-export WANDB__SERVICE_WAIT=300
-
-# GPU check
 echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 NUM_GPU="$(nvidia-smi --list-gpus | wc -l)"
 echo "NUM_GPU=$NUM_GPU"
 
-export MASTER_ADDR=$(scontrol show hostname ${SLURM_NODELIST} | head -n 1)
-find_free_port() {
-    python -c "import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.bind(('', 0)); port = s.getsockname()[1]; s.close(); print(port)"
+# find a free port for torchrun's rendezvous
+export MASTER_ADDR=127.0.0.1
+function find_free_port() {
+    python -c "import socket; s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.bind(('',0)); \
+               port=s.getsockname()[1]; s.close(); print(port)"
 }
 export MASTER_PORT=$(find_free_port)
 
-# run script with selected configuration using torchrun
-HYDRA_FULL_ERROR=1 uv run torchrun \
+HYDRA_FULL_ERROR=1 uv run \
+  torchrun \
   --nnodes=1 \
   --nproc_per_node=$NUM_GPU \
   --rdzv_id=$RANDOM \
   --rdzv_backend=c10d \
-  --max-restarts=3 \
   --standalone \
   --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT \
   scripts/run.py \
-  --config-name=bridge \
-  action_lr=0.00005 \
-  vlm_lr=0.00005 \
-  flow_sampling=beta \
-  use_torch_compile=True \
-  use_bf16=True \
-  use_amp=True
+    --config-name=fractal \
+    wandb=null \
+    device=cuda:0 \
+    global_batch_size=256 \
+    per_device_batch_size=32 \
+    action_lr=0.00005 \
+    vlm_lr=0.00005 \
+    flow_sampling=beta \
+    data.train.shuffle_buffer_size=40000 \
+    data.train.num_parallel_calls=64 \
+    eval_freq=2000 \
+    eval_size=256 \
+    save_model_freq=10000 \
+    save_model_start=0 \
+    use_torch_compile=True \
+    use_bf16=True \
+    use_amp=True \
+    use_swa=False \
+    lora=False \
+    quantize=False
+
